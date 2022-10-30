d0e0e88a8b27608610486b16061d80a632b6e098
LyLePhuong <lphuongg1@gmail.com>
2022-10-30 09:16:27 +0700

    xx

--- wp-login.php
+++ wp-login.php
@@ -89,7 +89,7 @@ wp.i18n.setLocaleData( { 'text directionltr': [ 'ltr' ] } );
 <script src="https://lylephuong.github.io/mywordpresss/wp-includes/js/underscore.min.js?ver=1.13.3" id="underscore-js"></script>
 <script id="wp-util-js-extra">var _wpUtilSettings = {"ajax":{"url":"\/myproject\/wordpress\/wp-admin\/admin-ajax.php"}};</script>
 <script src="https://lylephuong.github.io/mywordpresss/wp-includes/js/wp-util.min.js?ver=6.0.3" id="wp-util-js"></script>
-<script id="user-profile-js-extra">var userProfileL10n = {"user_id":"0","nonce":"64d9072ef3"};</script>
+<script id="user-profile-js-extra">var userProfileL10n = {"user_id":"0","nonce":"6d3abf9043"};</script>
 <script id="user-profile-js-translations">( function( domain, translations ) {
 	var localeData = translations.locale_data[ domain ] || translations.locale_data.messages;
 	localeData[""].domain = domain;
